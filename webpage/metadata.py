f6bda50d9c942270b5eb43c63ea781430f4ee6da
csae8092 <peter.andorfer@oeaw.ac.at>
2021-10-29 13:54:49 +0200

    init commit

---
+++ webpage/metadata.py
@@ -0,0 +1,19 @@
+# this files contains basic metadata about the project. This data will be used
+# (by default) in the base.html and index.html
+
+PROJECT_METADATA = {
+    'title': 'Fingerprinting White Marbles',
+    'author': 'Peter Andorfer',
+    'subtitle': 'QUARRIES AND CITIES OF ROMAN THRACE, 1ST-3RD CENTURY AD',
+    'description': 'lorem ipsum',
+    'github': 'https://github.com/acdh-oeaw/fwm',
+    'version': '0.0.1',
+    'matomo_id': 'provide some',
+    'matomo_url': '//matomo.acdh.oeaw.ac.at/',
+    'imprint': '/imprint',
+    'social_media': [
+        ('fab fa-twitter fa-2x', 'https://twitter.com/ACDH_OeAW'),
+        ('fab fa-youtube fa-2x', 'https://www.youtube.com/channel/UCgaEMaMbPkULYRI5u6gvG-w'),
+    ],
+    'app_type': 'database',  # database|website|service|tool|digital-edition
+}
